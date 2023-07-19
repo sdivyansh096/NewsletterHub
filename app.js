b646f9a061bc66bd78008490fe890fc65a93c5e9
Divyansh Sharma <138120857+sdivyansh096@users.noreply.github.com>
2023-07-19 13:39:46 +0530

    Update app.js
    updated path of public directory

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const https = require("https");
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.get("/",function(req,res){
     res.sendFile(__dirname+"/signup.html")
